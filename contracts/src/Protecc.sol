7ae7f06257ed12e1d79e3599b05424b4215b0f28
mehranhydary <mehranhydary@users.noreply.github.com>
2023-10-16 20:51:25 -0400

    Figuring out how simple diamond proxy works

--- contracts/src/Protecc.sol
+++ contracts/src/Protecc.sol
@@ -15,8 +15,13 @@ import {Pool} from "@uniswap/v4-core/contracts/libraries/Pool.sol";
 import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
 import {Position} from "@uniswap/v4-core/contracts/libraries/Position.sol";
 import {LiquidityAmounts} from "periphery-next/libraries/LiquidityAmounts.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {SafeCast} from "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Protecc is BaseHook {
+contract Protecc is BaseHook, ERC20, IERC1155Receiver, ReentrancyGuard {
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
     using BalanceDeltaLibrary for BalanceDelta;
@@ -45,16 +50,101 @@ contract Protecc is BaseHook {
     error TotalSupplyZero();
     error InvalidCurrencyDelta();
 
+    uint24 internal constant _PIPS = 1000000;
+
+    int24 public immutable lowerTick;
+    int24 public immutable upperTick;
+    int24 public immutable tickSpacing;
+    uint24 public immutable baseBeta; // % expressed as uint < 1e6
+    uint24 public immutable decayRate; // % expressed as uint < 1e6
+    uint24 public immutable vaultRedepositRate; // % expressed as uint < 1e6
+
+    /// @dev these could be TRANSIENT STORAGE eventually
+    uint256 internal _a0;
+    uint256 internal _a1;
+    /// ----------
+
+    uint256 public lastBlockOpened;
+    uint256 public lastBlockReset;
+    uint256 public hedgeRequired0;
+    uint256 public hedgeRequired1;
+    uint256 public hedgeCommitted0;
+    uint256 public hedgeCommitted1;
+    uint160 public committedSqrtPriceX96;
+    PoolKey public poolKey;
+    address public committer;
+    bool public initialized;
+
+    struct PoolManagerCalldata {
+        uint256 amount; /// mintAmount | burnAmount | newSqrtPriceX96 (inferred from actionType)
+        address msgSender;
+        address receiver;
+        uint8 actionType; /// 0 = mint | 1 = burn | 2 = arbSwap
+    }
+
+    struct ArbSwapParams {
+        uint160 sqrtPriceX96;
+        uint160 newSqrtPriceX96;
+        uint160 sqrtPriceX96Lower;
+        uint160 sqrtPriceX96Upper;
+        uint128 liquidity;
+        uint24 betaFactor;
+    }
+
     constructor(
         IPoolManager _poolManager,
         int24 _tickSpacing,
         uint24 _baseBeta,
         uint24 _decayRate,
         uint24 _vaultRedepositRate
-    ) BaseHook(_poolManager) {
+    ) BaseHook(_poolManager) ERC20("Diamond LP Token", "DLPT") {
         lowerTick = _tickSpacing.minUsableTick();
         upperTick = _tickSpacing.maxUsableTick();
         tickSpacing = _tickSpacing;
+        require(
+            _baseBeta < _PIPS &&
+                _decayRate <= _baseBeta &&
+                _vaultRedepositRate < _PIPS
+        );
+        baseBeta = _baseBeta;
+        decayRate = _decayRate;
+        vaultRedepositRate = _vaultRedepositRate;
+    }
+
+    function onERC1155Received(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) external view returns (bytes4) {
+        if (msg.sender != address(poolManager)) revert NotPoolManagerToken();
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155Received(address,address,uint256,uint256,bytes)"
+                )
+            );
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external view returns (bytes4) {
+        if (msg.sender != address(poolManager)) revert NotPoolManagerToken();
+        return
+            bytes4(
+                keccak256(
+                    "onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"
+                )
+            );
+    }
+
+    function supportsInterface(bytes4) external pure returns (bool) {
+        return true;
     }
 
     function getHooksCalls() public pure override returns (Hooks.Calls memory) {
