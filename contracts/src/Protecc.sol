3f4e78b6d85e1d9e2269ae00433624ef0ad5ed0e
mehranhydary <mehranhydary@users.noreply.github.com>
2023-10-16 20:44:25 -0400

    Adding libraries

--- contracts/src/Protecc.sol
+++ contracts/src/Protecc.sol
@@ -1,4 +1,102 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-contract Protecc {}
+import {BaseHook} from "periphery-next/BaseHook.sol";
+import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/contracts/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "@uniswap/v4-core/contracts/types/PoolId.sol";
+import {BalanceDelta, BalanceDeltaLibrary} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
+import {FeeLibrary} from "@uniswap/v4-core/contracts/libraries/FeeLibrary.sol";
+import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Currency.sol";
+import {FullMath} from "@uniswap/v4-core/contracts/libraries/FullMath.sol";
+import {TickMath} from "@uniswap/v4-core/contracts/libraries/TickMath.sol";
+import {Pool} from "@uniswap/v4-core/contracts/libraries/Pool.sol";
+import {PoolManager} from "@uniswap/v4-core/contracts/PoolManager.sol";
+import {Position} from "@uniswap/v4-core/contracts/libraries/Position.sol";
+import {LiquidityAmounts} from "periphery-next/libraries/LiquidityAmounts.sol";
+
+contract Protecc is BaseHook {
+    using PoolIdLibrary for PoolKey;
+    using CurrencyLibrary for Currency;
+    using BalanceDeltaLibrary for BalanceDelta;
+    using FeeLibrary for uint24;
+    using TickMath for int24;
+    using Pool for Pool.State;
+    using SafeERC20 for ERC20;
+    using SafeERC20 for PoolManager;
+
+    error AlreadyInitialized();
+    error NotPoolManagerToken();
+    error InvalidTickSpacing();
+    error InvalidMsgValue();
+    error OnlyModifyViaHook();
+    error PoolAlreadyOpened();
+    error PoolNotOpen();
+    error ArbTooSmall();
+    error LiquidityZero();
+    error InsufficientHedgeCommitted();
+    error MintZero();
+    error BurnZero();
+    error BurnExceedsSupply();
+    error WithdrawExceedsAvailable();
+    error OnlyCommitter();
+    error PriceOutOfBounds();
+    error TotalSupplyZero();
+    error InvalidCurrencyDelta();
+
+    constructor(
+        IPoolManager _poolManager,
+        int24 _tickSpacing,
+        uint24 _baseBeta,
+        uint24 _decayRate,
+        uint24 _vaultRedepositRate
+    ) BaseHook(_poolManager) {
+        lowerTick = _tickSpacing.minUsableTick();
+        upperTick = _tickSpacing.maxUsableTick();
+        tickSpacing = _tickSpacing;
+    }
+
+    function getHooksCalls() public pure override returns (Hooks.Calls memory) {
+        return
+            Hooks.Calls({
+                beforeInitialize: true,
+                afterInitialize: false,
+                beforeModifyPosition: true,
+                afterModifyPosition: false,
+                beforeSwap: true,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false
+            });
+    }
+
+    function beforeInitialize(
+        address,
+        PoolKey calldata,
+        uint160,
+        bytes calldata
+    ) external override returns (bytes4) {}
+
+    function beforeModifyPosition(
+        address,
+        PoolKey calldata,
+        IPoolManager.ModifyPositionParams calldata,
+        bytes calldata
+    ) external override returns (bytes4) {}
+
+    function beforeSwap(
+        address,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata,
+        bytes calldata
+    ) external override returns (bytes4) {}
+
+    function afterSwap(
+        address,
+        PoolKey calldata,
+        IPoolManager.SwapParams calldata,
+        BalanceDelta,
+        bytes calldata
+    ) external override returns (bytes4) {}
+}
