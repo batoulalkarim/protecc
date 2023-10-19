27eebcbe379ada7e45b8cd7fa75e89a8b115c453
mehranhydary <mehranhydary@users.noreply.github.com>
2023-10-19 01:31:37 -0400

    Adding more specific comments

--- contracts/src/NewIdea.sol
+++ contracts/src/NewIdea.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.19;
+pragma solidity 0.8.20;
 
 import {BaseHook} from "periphery-next/BaseHook.sol";
 import {Hooks} from "@uniswap/v4-core/contracts/libraries/Hooks.sol";
@@ -9,12 +9,17 @@ import {CurrencyLibrary, Currency} from "@uniswap/v4-core/contracts/types/Curren
 import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
 import {BalanceDelta} from "@uniswap/v4-core/contracts/types/BalanceDelta.sol";
 
+import {Constants} from "./libraries/Constants.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract NewIdea is BaseHook {
     using PoolIdLibrary for PoolKey;
     using CurrencyLibrary for Currency;
 
-    address public immutable savingsDai =
-        0x83F20F44975D03b1b09e64809B757c47f942BEeA;
+    address public immutable savingsDai = (
+        0x83F20F44975D03b1b09e64809B757c47f942BEeA
+    );
+    address public immutable dai = (0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
     constructor(IPoolManager _poolManager) BaseHook(_poolManager) {}
 
@@ -38,17 +43,26 @@ contract NewIdea is BaseHook {
         IPoolManager.SwapParams calldata params,
         bytes calldata
     ) external view override returns (bytes4) {
-        // Need a function to handle ETH
-        //   If user is sending token and receiving ETH
-        //   If user is sending ETH and receiving token
+        if (params.zeroForOne) {
+            if (poolKey.currency0 == dai) {
+                // User is swapping from dai to token
+                // Do nothing (handled afterSwap)
+            } else {
+                // User is swapping from token to dai
+                // Take the existing sDAI, unwind it (to DAI; compare with SwapParams) and give it to user
+                // Whatever is left, convert it back to sDAI
+            }
+        } else {
+            if (poolKey.currency1 == dai) {
+                // User is swapping from dai to token
+                // Do nothing (handled afterSwap)
+            } else {
+                // User is swapping from token to dai
+                // Take the existing sDAI, unwind it (to DAI; compare with SwapParams) and give it to user
+                // Whatever is left, convert it back to sDAI
+            }
+        }
 
-        // Need a function to handle DAI
-        //   If user is sending token and receiving DAI
-        //     - unwind the sDAI, give back some DAI and give
-        //     - the yield to the LP (how do I know who is LPing?)
-        //   If user is sending DAI and receiving token
-        //     - do nothing I guess (will be handled in afterSwap)... or
-        //     - wrap the DAI so that it becomes sDAI
         return BaseHook.beforeSwap.selector;
     }
 
@@ -59,6 +73,23 @@ contract NewIdea is BaseHook {
         BalanceDelta delta,
         bytes calldata hookData
     ) external override returns (bytes4) {
+        if (params.zeroForOne) {
+            if (poolKey.currency0 == dai) {
+                // User is swapping from dai to token
+                // There is now excess dai in this pool, swap it for sDAI
+            } else {
+                // User is swapping from token to dai
+                // Do nothing
+            }
+        } else {
+            if (poolKey.currency1 == dai) {
+                // User is swapping from dai to token
+                // Do nothing
+            } else {
+                // User is swapping from token to dai
+                // There is now excess dai in this pool, swap it for sDAI
+            }
+        }
         return BaseHook.afterSwap.selector;
     }
 
---
+++ contracts/src/libraries/Constants.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity 0.8.20;
+
+library Constants {
+    address internal constant ETH = address(0);
+}
